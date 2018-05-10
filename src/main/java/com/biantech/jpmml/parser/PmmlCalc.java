fab2a03c499b9446b36444426a4683ad2a7100a0
Jonathan Bian <biantech@163.com>
2018-05-10 16:09:32 +0800

    Update PmmlCalc.java

--- src/main/java/com/biantech/jpmml/parser/PmmlCalc.java
+++ src/main/java/com/biantech/jpmml/parser/PmmlCalc.java
@@ -1,80 +1,79 @@
 package com.biantech.jpmml.parser;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
-import java.util.Set;
 
-import com.google.common.io.Files;
+import javax.xml.bind.JAXBException;
+
 import org.dmg.pmml.FieldName;
+import org.dmg.pmml.PMML;
+import org.jpmml.evaluator.ModelEvaluator;
+import org.jpmml.evaluator.ModelEvaluatorFactory;
+import org.jpmml.model.PMMLUtil;
+import org.xml.sax.SAXException;
 
 /**
- * 使用模型
+ * 读取pmml 获取模型
  * @author biantech
  *
  */
-public class PmmlCalc {
-	final static String utf8="utf-8";
-	public static void main(String[] args) throws IOException {
-		if(args.length < 2){
-			System.out.println("参数个数不匹配");
-		}
-		 //文件生成路径   
-        //PrintStream ps=new PrintStream("result.txt");
-        //System.setOut(ps);
-		String pmmlPath = args[0];  
-		String modelArgsFilePath = args[1];
-		PmmlInvoker invoker = new PmmlInvoker(pmmlPath);
-		List<Map<FieldName, String>> paramList = readInParams(modelArgsFilePath);
-		int lineNum = 0;  //当前处理行数
-		File file = new File("result.txt");
-		for(Map<FieldName, String> param : paramList){
-		 lineNum++;
-		 //System.out.println("======当前行： " + lineNum + "=======");
-		 Files.append("======当前行： " + lineNum + "=======",file,Charset.forName(utf8));
-		 Map<FieldName, ?> result = invoker.invoke(param);
-		 Set<FieldName> keySet = result.keySet();  //获取结果的keySet
-		 for(FieldName fn : keySet){
-		 	String tempString = result.get(fn).toString()+"\n";
-			Files.append(tempString,file,Charset.forName(utf8));
-		 	//System.out.println(result.get(fn).toString());
-		 }
-		 //Files.append(""
-		}
-	}
-	
-	/**
-	 * 读取参数文件
-	 * @param filePath 文件路径
-	 * @return
-	 * @throws IOException 
-	 */
-	public static List<Map<FieldName,String>> readInParams(String filePath) throws IOException{
-		InputStream is;
-		is = PmmlCalc.class.getClassLoader().getResourceAsStream(filePath);
-		if(is==null){
-			is = new FileInputStream(filePath);
-		}
-		InputStreamReader isreader = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isreader);
-		//BufferedReader br = new BufferedReader(new FileReader(filePath));
-		String[] nameArr = br.readLine().split(",");  //读取表头的名字
-		ArrayList<Map<FieldName,String>> list = new ArrayList<>();
-		String paramLine;  //一行参数
-		//循环读取  每次读取一行数据
-		while((paramLine = br.readLine()) != null){
-			Map<FieldName,String> map = new HashMap<>();
-			String[] paramLineArr = paramLine.split(",");
-			for(int i=0; i<paramLineArr.length; i++){//一次循环处理一行数据
-				map.put(new FieldName(nameArr[i]), paramLineArr[i]); //将表头和值组成map 加入list中
+public class PmmlInvoker {
+	private ModelEvaluator modelEvaluator;
+
+	// 通过文件读取模型
+	public PmmlInvoker(String pmmlFileName) {
+		PMML pmml = null;
+		InputStream is = null;
+		try {
+			if (pmmlFileName != null) {
+				is = PmmlInvoker.class.getClassLoader().getResourceAsStream(pmmlFileName);
+				if(is==null){
+					is = new FileInputStream(pmmlFileName);
+				}
+				pmml = PMMLUtil.unmarshal(is);
 			}
-			list.add(map);
+			this.modelEvaluator = ModelEvaluatorFactory.newInstance().newModelEvaluator(pmml);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(is!=null)
+					is.close();
+			} catch (Exception localIOException3) {
+				localIOException3.printStackTrace();
+			}
 		}
-		is.close();
-		return list;
+		this.modelEvaluator.verify();
+		System.out.println("模型读取成功");
+	}
+
+	// 通过输入流读取模型
+	public PmmlInvoker(InputStream is) {
+		PMML pmml;
+		try {
+			pmml = PMMLUtil.unmarshal(is);
+			try {
+				is.close();
+			} catch (IOException localIOException) {
+
+			}
+			this.modelEvaluator = ModelEvaluatorFactory.newInstance().newModelEvaluator(pmml);
+		} catch (SAXException e) {
+			pmml = null;
+		} catch (JAXBException e) {
+			pmml = null;
+		} finally {
+			try {
+				is.close();
+			} catch (IOException localIOException3) {
+			}
+		}
+		this.modelEvaluator.verify();
 	}
 	
+	public Map<FieldName, String> invoke(Map<FieldName, String> paramsMap) {
+		return this.modelEvaluator.evaluate(paramsMap);
+	}
 }
